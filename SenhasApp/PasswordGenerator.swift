7510d330c44082267334091630efaaf469adb5f2
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-02-21 17:53:46 -0400

    parte logica do gerador de senhas

--- SenhasApp/PasswordGenerator.swift
+++ SenhasApp/PasswordGenerator.swift
@@ -33,4 +33,36 @@ class PasswordGenerator {
         self.useSpecialCharacters = useSpecialCharacters
     }
     
+    
+    func generate(total: Int) -> [String] {
+        passwords.removeAll()
+
+        var universe: String = "" // variavel para armazenar o universo de caracters a ser utilizado para gerar as senhas
+        
+        // IFS para selecionar os caracters selecionados pelo usuario.
+        if userLetters {
+            universe += letters
+        }
+        if useNumbers {
+            universe += numbers
+        }
+        if useSpecialCharacters {
+            universe += specialCharacterss
+        }
+        if useCapitalLetters {
+            universe += letters.uppercased() //letras maiusculas
+        }
+        
+        let universeArray = Array(universe) //um array contendo cada um caracter que esta selecionado
+        
+        while passwords.count < total {
+            var password = ""
+            for _ in 1...numberOfCharacters {
+                let index = Int(arc4random_uniform(UInt32(universeArray.count)))
+                password += String(universeArray[index]) //converto a letra para string porque ela e do tipo caracter.
+            }
+            passwords.append(password) // adiciona na variavel ate o numero de senhas ser menor que a quantidade informada.
+        }
+        return passwords
+    }
 }
